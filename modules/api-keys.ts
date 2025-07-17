6c26516a22b00c46a78baea2f3f0113e7b933559
mosch <mo@zuplo.com>
2025-07-17 08:58:24 +0000

    Add API key policy

--- modules/api-keys.ts
+++ modules/api-keys.ts
@@ -1,25 +1,35 @@
 import { ZuploContext, ZuploRequest, environment, apiServices } from "@zuplo/runtime";
 
+async function maybeJson(request) {
+  try {
+    return request.json()
+  } catch {
+    return {}
+  }
+}
+
 export default async function (request: ZuploRequest, context: ZuploContext) {
   const accountName = environment.ZUPLO_ACCOUNT_NAME;
   const bucketId = environment.BUCKET_ID ??  "bckt_2nt4EJySjm6c8fUXMzLEsOdY9nqKzHIHM" ?? "bckt_17q5xYxVVtW88VW7UMHx7WykOnHXv2HoS";
 
   const sub = request.user?.sub;
   const userClaims = request.user?.data;
+  const body = await maybeJson(request)
 
   await apiServices(`/v2/key-auth/${bucketId}/consumers`, {
     method: "POST",
     data: {
       name: crypto.randomUUID(),
-      description: "API Key",
+      description: body.description ?? "API Key",
       metadata: {
+        // Pass any metadata
         squareSpaceId: userClaims.squareSpaceId ?? "anon",
       },
       tags: {},
       managers: [
         {
-          // email: "",
-          sub, // if no sub is passed, an invite will be created
+          // email: "", // Optionally pass the users email
+          sub, // This links the identities
         }
       ]
     },
